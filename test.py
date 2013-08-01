fdf58f25884fe69835a38faf6df79c53de77b0ea
gmorell <gmp@h4xmb.org>
2013-08-01 18:56:52 -0400

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+###
+# Copyright (c) 2013, Gabriel Morell-Pacheco
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+class RGBLEDsTestCase(PluginTestCase):
+    plugins = ('RGBLEDs',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
